d2619145bb029487b5e32e1a12aad01aaeda1f80
Chbe19 <chbe19@student.bth.se>
2022-12-16 16:15:05 +0100

    Added moving ricardo

--- app/src/main/java/com/bignerdranch/android/maptest/profile.kt
+++ app/src/main/java/com/bignerdranch/android/maptest/profile.kt
@@ -1,10 +1,14 @@
 package com.bignerdranch.android.maptest
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.viewbinding.ViewBindings
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +24,10 @@ class profile : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var maxScore : Int = 100
+    private var score : Int = 40 // From database object setScore
+    private var nextReward: Int = maxScore - score
+    private var userName: String = "Charlie Bergh" // From database object getName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,7 @@ class profile : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -35,6 +44,15 @@ class profile : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var progressBar = view.findViewById<ProgressBar>(R.id.pbscore)
+        progressBar.max = maxScore
+        progressBar.setProgress(score)
+        view.findViewById<TextView>(R.id.textUser).setText(userName)
+        view.findViewById<TextView>(R.id.textScore).setText("Current Score: " + score + "/" + maxScore)
     }
 
     companion object {
