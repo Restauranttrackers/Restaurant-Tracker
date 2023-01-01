ac69f3592808359ad8f71fa91effde176fb49f0a
Chbe19 <chbe19@student.bth.se>
2023-01-01 16:45:44 +0100

    Picasso Dependency
    Fix

--- app/src/main/java/com/bignerdranch/android/maptest/profile.kt
+++ app/src/main/java/com/bignerdranch/android/maptest/profile.kt
@@ -144,14 +144,14 @@ class profile : Fragment() {
          * @return A new instance of fragment map.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
+/**@JvmStatic
         fun newInstance(param1: String, param2: String) =
             map().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
-            }
+            }**/
     }
 }
 
