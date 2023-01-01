bea3317815216910730930ad2f3bfd5ead8cbe5d
Elrulia <47306106+Elrulia@users.noreply.github.com>
2023-01-01 17:43:37 +0100

    Added onlistener to buttonSetting
    Added onclicklistener on buttonSettings that loads the setting.kt. currently a copy of list.
    
    Added fragment layout file for settings but unused as the setting.kt loads the list fragment etc.

---
+++ app/src/main/java/com/bignerdranch/android/maptest/Setting.kt
@@ -0,0 +1,98 @@
+package com.bignerdranch.android.maptest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.maptest.MapsActivity.Companion.data
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [list.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Setting : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var adapter: CardAdapter
+    private lateinit var recyclerView: RecyclerView
+    private var restaurantsArrayList: MutableList<Restaurants> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment list.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            list().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getRestaurantData()
+
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        adapter = CardAdapter(restaurantsArrayList, data)
+        recyclerView.adapter = adapter
+    }
+
+    private fun getRestaurantData() {
+        //Log.d("inne i emilias saker", "${data.listRestaurants}")
+        for(document in data.flexibleRestaurantList!!) {
+            dataInitialize(document.id as String, document.name as String, document.status as String, document.info as String, document.description as String, document.image as String)
+        }
+    }
+
+    private fun dataInitialize(id: String, name: String, status: String, info: String, description: String, image_url: String) {
+        val imageId: String = image_url
+        val restaId: String = id
+        val restaName : String = name
+        val restaInfo : String = info
+        val restaDescr : String = description
+        val mark: String = status
+
+        val restaurants = Restaurants(restaId, imageId, restaName, restaInfo, restaDescr, mark)
+        restaurantsArrayList.add(restaurants)
+    }
+}
--- app/src/main/java/com/bignerdranch/android/maptest/profile.kt
+++ app/src/main/java/com/bignerdranch/android/maptest/profile.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.maptest
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,9 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
 import pl.droidsonroids.gif.GifImageView
+
+
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,6 +57,7 @@ class profile : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val progressBar = view.findViewById<ProgressBar>(R.id.pbscore)
 
+
         createRewards(rewardContainer)
         progressBar.max = maxScore
         progressBar.progress = score
@@ -89,6 +94,16 @@ class profile : Fragment() {
                 view.findViewById<TextView>(R.id.reward3).text = tracker
                 pb.max = requirement
                 pb.progress = progress
+            }
+        }
+
+        val settingsFragmentButton: Button = view.findViewById(R.id.buttonSettings)
+        settingsFragmentButton.setOnClickListener {
+            val fragmentManager = fragmentManager
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            if (fragmentTransaction != null) {
+                fragmentTransaction.replace(R.id.frame_layout, Setting(), "currentFragment")
+                fragmentTransaction.commit()
             }
         }
     }
@@ -153,5 +168,6 @@ class profile : Fragment() {
                 }
             }**/
     }
+
 }
 
