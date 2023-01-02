13d58db17ee30a7d4a83d83fb81c69e46435f9d6
Chbe19 <chbe19@student.bth.se>
2023-01-02 14:04:21 +0100

    Flyttat kod

--- app/src/main/java/com/bignerdranch/android/maptest/profile.kt
+++ app/src/main/java/com/bignerdranch/android/maptest/profile.kt
@@ -2,7 +2,6 @@ package com.bignerdranch.android.maptest
 
 
 import android.os.Bundle
-import android.provider.ContactsContract.Data
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,16 +9,10 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
 import pl.droidsonroids.gif.GifImageView
-import com.bignerdranch.android.maptest.MapsActivity.Companion.data
-
-
-
+import models.ProfileController
 import android.widget.Button
+import models.Rewards
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -29,8 +22,6 @@ private const val ARG_PARAM2 = "param2"
 class profile : Fragment() {
     private var maxScore: Int = 100
     private var score: Int = 0
-    private var userName: String? = null
-    private var title: String? = null
     private var picture = R.drawable.bavatogay_ricardo_milos // TODO From Json/db = NULL
     private var border = R.drawable.rainbow_color_colorful // TODO From Json/db = NULL
     private var rewardContainer = ArrayList<Rewards>()
@@ -43,26 +34,25 @@ class profile : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    /**
+     * Initializes all drawable objects
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val progressBar = view.findViewById<ProgressBar>(R.id.pbscore)
+        val user = ProfileController(picture,border)
+        this.score = user.getScore()
+        val rewards = Rewards(progress = this.score)
 
-
-        data.getUser()
-        setName()
-        setScore()
-        setTitle()
-        setPicture()
-        setBorder()
-        createRewards(rewardContainer)
+        rewardContainer = rewards.createRewards(rewardContainer)
         progressBar.max = maxScore
-        progressBar.progress = score
-        view.findViewById<TextView>(R.id.textUser).text = userName
+        progressBar.progress = this.score
+        view.findViewById<TextView>(R.id.textUser).text = user.getUsername()
         "Current Score: $score/$maxScore".also { view.findViewById<TextView>(R.id.textScore)
             .text = it }
-        view.findViewById<TextView>(R.id.textTitle).text = title
-        view.findViewById<GifImageView>(R.id.imageUser).setImageResource(picture)
-        view.findViewById<GifImageView>(R.id.imageUserBorder).setImageResource(border)
+        view.findViewById<TextView>(R.id.textTitle).text = user.getTitle()
+        view.findViewById<GifImageView>(R.id.imageUser).setImageResource(user.getPicture())
+        view.findViewById<GifImageView>(R.id.imageUserBorder).setImageResource(user.getBorder())
         // Routine to update / Add reward to user
         rewardContainer.forEach{
             val name: String = it.name
@@ -75,7 +65,7 @@ class profile : Fragment() {
                 // TODO Add reward to user
             }
             if( name == "reward1"){
-                val pb = view.findViewById<ProgressBar>(R.id.pbreward3)
+                val pb = view.findViewById<ProgressBar>(R.id.pbreward1)
                 view.findViewById<TextView>(R.id.reward1).text = tracker
                 pb.max = requirement
                 pb.progress = progress
@@ -102,79 +92,6 @@ class profile : Fragment() {
                 fragmentTransaction.replace(R.id.frame_layout, Setting(), "currentFragment")
                 fragmentTransaction.commit()
             }
-        }
-    }
-    /**
-     * Initializes the name from database
-     */
-    private fun setName(){
-    this.userName = (data.listUser[0].firstname as String) + " " + (data.listUser[0].lastname as String)
-    }
-    /**
-     * Initializes the score from database
-     */
-    private fun setScore(){
-    this.score = data.listUser[0].score as Int
-    }
-    /**
-     * Initializes the title from database
-     */
-    private fun setTitle(){
-    this.title = data.listUser[0].banner as String
-    }
-    /**
-     * Initializes the picture from settings
-     */
-    private fun setPicture(){
-    //TODO: Get from settings json
-    }
-    /**
-     * Initializes the border from settings
-     */
-    private fun setBorder(){
-    //TODO: Get from settings json
-    }
-
-    // TODO: Move to separate module
-    class Rewards(
-        var name: String = "", var tracker: String = "",
-        var progress: Int = 0, var requirement: Int = 0, var complete: Boolean = false
-    )
-
-    /**
-    * Creates Rewards objects and pushes them to the container.
-    * @param ArrayList of type Rewards.
-     */
-    private fun createRewards(rewardContainer: ArrayList<Rewards>) {
-        var requirement= 20
-        var tracker= ""
-        if (score >= requirement) {
-            val reward1 = Rewards("reward1", "Completed!", score, requirement, true)
-            rewardContainer.add(reward1)
-        } else {
-            tracker = "$score/$requirement"
-            val reward1 = Rewards("reward1", tracker, score, requirement, false)
-            rewardContainer.add(reward1)
-        }
-        requirement = 40
-        tracker = ""
-        if (score >= requirement) {
-            val reward2 = Rewards("reward2", "Completed!", score, requirement, true)
-            rewardContainer.add(reward2)
-        } else {
-            tracker = "$score/$requirement"
-            val reward2 = Rewards("reward2", tracker, score, requirement, false)
-            rewardContainer.add(reward2)
-        }
-        requirement = 75
-        tracker = ""
-        if (score >= requirement) {
-            val reward3 = Rewards("reward3", "Completed!", score, requirement, true)
-            rewardContainer.add(reward3)
-        } else {
-            tracker = "$score/$requirement"
-            val reward3 = Rewards("reward3", tracker, score, requirement, false)
-            rewardContainer.add(reward3)
         }
     }
 
---
+++ app/src/main/java/models/ProfileController.kt
@@ -0,0 +1,29 @@
+package models
+
+import com.bignerdranch.android.maptest.MapsActivity.Companion.data
+
+/**
+ * Initializes user from database. Used in profile fragment.
+ */
+  class ProfileController(private var picture: Int, private var border: Int) {
+      private var score: Int = data.listUser[0].score as Int
+      private var userName: String = (data.listUser[0].firstname as String) + " " + (data.listUser[0].lastname as String)
+      private var title: String = data.listUser[0].banner as String
+
+      public fun getBorder(): Int{
+          return this.border
+      }
+      public fun getPicture(): Int{
+          return this.picture
+      }
+      public fun getScore(): Int{
+          return this.score
+      }
+      public fun getUsername(): String{
+          return this.userName
+      }
+      public fun getTitle(): String{
+          return this.title
+      }
+
+ }
